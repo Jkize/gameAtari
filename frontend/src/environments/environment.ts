01316d3d13c4a43371d257f6b1fb033354c401f8
Jkize :3 <41697595+Jkize@users.noreply.github.com>
2026-07-12 00:21:38 -0500

    login google done

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   backendUrl: `http://${window.location.hostname}:3000`,
-  googleClientId: 'local-google-client-id.apps.googleusercontent.com',
+  googleClientId: '224541701716-c8j0tj7132v9i6smsn5bt86v0ao932vp.apps.googleusercontent.com',
   devGameMode: false,
   interpolationEnabled: true,
 };
